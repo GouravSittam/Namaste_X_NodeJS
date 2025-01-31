94066c913909e132010f3a79a2d5390a069c88b5
Gourav Chaudhary <gouravsittam@gmail.com>
2025-01-31 19:48:47 +0530

    Add initial implementation of module exports with sum calculation and logging

---
+++ modules.export & require/app.js
@@ -0,0 +1,10 @@
+require("./xyz.js");
+const { x, calculateSum } = require("./sum.js");
+
+var name = "Namaste NodeJs";
+
+var a = 12;
+var b = 54;
+
+console.log(x);
+console.log(calculateSum(a, b));
---
+++ modules.export & require/sum.js
@@ -0,0 +1,13 @@
+console.log("Sum Two Numbers");
+
+var x = "Hello World";
+
+function calculateSum(a, b) {
+    const sum = a + b;
+    console.log(sum);
+}
+
+module.exports = {
+    x,
+    calculateSum,
+}
