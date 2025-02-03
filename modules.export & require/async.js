ae5a76e2e269bb5692d457eb306bc2059096b991
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-03 20:51:21 +0530

    Add asynchronous and synchronous module implementations for API data retrieval and multiplication

---
+++ modules.export & require/async.js
@@ -0,0 +1,20 @@
+const https = require('https');
+const fs = require('fs');
+
+https.get("https://api.fbi.com", (res) => {
+    res.on('data', (data) => {
+        console.log("Response Data:", data);
+    });
+});
+
+fs.readFile("./gossip.txt", "utf8", (err, data) => {
+    if (err) {
+        console.error("Error reading file:", err);
+        return;
+    }
+    console.log("File Data:", data);
+});
+
+setTimeout(() => {
+    console.log("Here for 5 seconds");
+}, 5000);
---
+++ modules.export & require/sync.js
@@ -0,0 +1,9 @@
+var a= 25454;
+var b=2465794;
+function mul(x,y){
+    const res=a*b;
+    return res;
+
+}
+var c=mul(a,b);
+console.log(c)
