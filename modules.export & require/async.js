6a39e9ef772c4a4bd746b01445802affacb92d3f
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-03 20:52:04 +0530

    Refactor async module for consistent formatting and improved readability

--- modules.export & require/async.js
+++ modules.export & require/async.js
@@ -1,20 +1,21 @@
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 
 https.get("https://api.fbi.com", (res) => {
-    res.on('data', (data) => {
-        console.log("Response Data:", data);
-    });
+  res.on("data", (data) => {
+    console.log("Response Data:", data);
+  });
 });
 
 fs.readFile("./gossip.txt", "utf8", (err, data) => {
-    if (err) {
-        console.error("Error reading file:", err);
-        return;
-    }
-    console.log("File Data:", data);
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  console.log("File Data:", data);
 });
 
 setTimeout(() => {
-    console.log("Here for 5 seconds");
+  console.log("Here for 5 seconds");
 }, 5000);
