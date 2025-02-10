f2ed72f6612efa822b655f1e971ae8e473c73270
Gourav Chaudhary <gouravsittam@gmail.com>
2025-02-11 00:04:50 +0530

    Add response for "/gobu" route to prevent access to secret data

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const server = http.createServer(function (req, res) {
+    if(req.url === "/gobu"){
+        res.end("There is no Secret data here");
+    }
   res.end("Hello World");
 });
 
